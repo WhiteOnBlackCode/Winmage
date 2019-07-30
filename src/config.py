12d82450e4f6f999252f27f35d6065cd2d71ed90
Maximus <max3227@gmail.com>
2019-07-31 02:08:12 +0300

    Added argument parsing (--force).The script will run once per day only.

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import configparser
 import pathlib
+from datetime import date, timedelta
 
 CONF_PATH = pathlib.WindowsPath('.') / 'config.ini'
 DEF_IMG_DIR = pathlib.WindowsPath('.') / 'img'
@@ -9,14 +10,30 @@ class Config(configparser.ConfigParser):
     def __init__(self):
         super(Config, self).__init__()
 
+        # Loading
         if CONF_PATH.is_file():
             self.read(CONF_PATH)
-            assert 'img_dir' in self[self.default_section]
+            if [x for x in ('img_dir', 'last_day') if x not in self[self.default_section]]:
+                self.generate()
         else:
-            self[self.default_section] = {'img_dir': DEF_IMG_DIR.resolve()}
-            with CONF_PATH.open('w') as f:
-                self.write(f)
+            self.generate()
 
         img_dir = pathlib.WindowsPath(self[self.default_section]['img_dir'])
         if not img_dir.exists():
             img_dir.mkdir()
+
+    def generate(self):
+        print('[+] Generating config.ini')
+        self[self.default_section] = {'img_dir': DEF_IMG_DIR.resolve(),
+                                      'last_day': date.today().isoformat()}
+        self.save()
+
+    def save(self):
+        self[self.default_section]['last_day'] = date.today().isoformat()
+
+        with CONF_PATH.open('w') as f:
+            self.write(f)
+
+    def should_run(self) -> bool:
+        last_day = date.fromisoformat(self[self.default_section]['last_day'])
+        return last_day + timedelta(days=1) <= date.today()
--- winmage.py
+++ winmage.py
@@ -6,13 +6,26 @@
 import os
 import pathlib
 import shutil
+from datetime import date, timedelta
 
 from src import *
 
 
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--force', default=False, action='store_true')
+    return parser.parse_args()
+
+
 class WinMage:
     def __init__(self):
-        self.conf = Config()['DEFAULT']
+        self.args = parse_args()
+        self.config = Config()
+        self.section = self.config[self.config.default_section]
+
+        if not self.args.force and not self.config.should_run():
+            exit('[!] Already ran today!')
 
         self.assets_dir = pathlib.WindowsPath(
             f'C:\\Users\\{os.getlogin()}\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets')
@@ -20,14 +33,16 @@ class WinMage:
             print('[X] No Spotlight directory found!')
             exit(2)
 
-        c_new = self.collectImages()
+        c_new = self.collect_images()
         print('[+] Added %d images' % c_new)
+        self.config.save()
 
-    def collectImages(self):
+    def collect_images(self):
         c = 0
         for fn in os.listdir(str(self.assets_dir)):
             asset_path = self.assets_dir / fn
-            target_path = pathlib.WindowsPath(self.conf['img_dir']) / (fn + '.jpg')
+            target_path = pathlib.WindowsPath(
+                self.section['img_dir']) / (fn + '.jpg')
 
             if target_path.exists():
                 continue
